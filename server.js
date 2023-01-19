c7ee636d91944e8e280f3a91ffca5bbefafda19d
Masud Rana <masudrana799dev@gmail.com>
2023-01-20 00:01:55 +0600

    configure database

--- server.js
+++ server.js
@@ -1,9 +1,48 @@
-const dotenv = require('dotenv');
-const app = require('./app');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-dotenv.config({ path: './config.env' });
+// Catching Uncaught Exceptions
+// this should be very top or before any code executing
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXCEPTION! 💥 Shutting down...");
+  console.log(err.name, err.message);
+  process.exit(1);
+});
 
+dotenv.config({ path: "./config.env" });
+const app = require("./app");
+
+// db connect
+const DB = process.env.DATABASE_LOCAL.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true
+  })
+  .then(() => console.log("DB connection successful!"));
+
+// listen server 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+// Handle Errors Outside Express Unhandled Rejections
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED REJECTION! 💥 Shutting down...");
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+// for heroku handle 24 hours restart
+process.on("SIGTERM", () => {
+  console.log("👋 SIGTERM RECEIVED. Shutting down gracefully");
+  server.close(() => {
+    console.log("💥 Process terminated!");
+  });
 });
